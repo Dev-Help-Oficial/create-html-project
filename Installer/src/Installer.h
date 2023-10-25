a70c3e3d6e61dbb9ee93f9e01f39079e7cceb9da
LESS14 <100519912+LESS14@users.noreply.github.com>
2023-10-25 14:34:23 -0500

    Adiciona instalador

---
+++ Installer/src/Installer.h
@@ -0,0 +1,13 @@
+﻿// Installer.h: arquivo de inclusão para arquivos de inclusão padrão do sistema,
+// ou arquivos de inclusão específicos a um projeto.
+
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <Windows.h>
+#include <shlobj.h>
+#include <fstream>
+
+// TODO: Referencie os cabeçalhos adicionais de que seu programa precisa aqui.
